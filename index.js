4620eaca4cccef4731e6b615c2bc03e8cf166661
Kenyetta Griffin <kenyettagriffin@pursuit.org>
2022-02-04 22:30:18 -0500

    "accumulator pattern lab"

--- index.js
+++ index.js
@@ -4,27 +4,15 @@
  * @returns {boolean} Returns `true` if all values in the array are numbers. Otherwise, return `false`.
  */
 function isValid(rolls) {
-for(const roll of rolls){
-  if(typeof(roll) === 'number'){ 
+  for (const roll of rolls) {
+    if (typeof roll === "number") {
+      return true;
+    } else {
+      return false;
+    }
+  }
   return true;
-}else{
-  return false;
-
 }
-}
-return true;
-};
-
-
-
-  
-
-    
-  
-
-
-
-
 
 /**
  * Finds a value in an array. If that value is in the array, returns it. Otherwise, returns `null`.
@@ -34,16 +22,15 @@ return true;
  */
 function findValue(rolls, value) {
   //let foundVal = [];
-  for(const found of rolls){ 
-    if(rolls.includes(value)){ 
-    return value;
-  }else{
-    return null;
+  for (const found of rolls) {
+    if (rolls.includes(value)) {
+      return value;
+    } else {
+      return null;
+    }
   }
-}
   return null;
 }
-
 
 /**
  * Returns a new array from the `rolls` array with only values equal to or greater than the `lowest` value.
@@ -53,23 +40,34 @@ function findValue(rolls, value) {
  */
 function filterOutLowValues(rolls, lowest) {
   newArr = [];
-  for(const roll of rolls){ 
-  if(roll >= lowest){ 
-    newArr.push(roll)
-  //return newArr(roll);
-}
+  for (const roll of rolls) {
+    if (roll >= lowest) {
+      newArr.push(roll);
+      //return newArr(roll);
+    }
   }
 
-
-return newArr;
+  return newArr;
 }
 /**
  * Returns an object which has rolls as keys and counts as values.
  * @param {number[]} rolls - An array of numbers representing rolls on a die.
  * @returns {object} An object where the keys are numbers rolled and the values are the number of times that roll appears in the `rolls` array.
  */
-function getRollCounts(rolls) {}
-
+function getRollCounts(rolls) {
+  const dieCount = {};
+  for (const roll of rolls) {
+    // console.log(roll);
+    if (dieCount[roll]) {
+      dieCount[roll] += 1;
+    } else {
+      dieCount[roll] = 1;
+    }
+  }
+  return dieCount;
+}
+//object[key] = value;
+//console.log(getRollCounts([2, 2, 3, 4, 3, 2]));
 // Do not change the code below here.
 module.exports = {
   isValid,
